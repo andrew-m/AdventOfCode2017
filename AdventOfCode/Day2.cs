e2e60f758ab3c879b533918523daed85e5166ad9
andrew-m <andrew-m@users.noreply.github.com>
2017-12-12 00:36:14 +0000

    Day 2 first half completed. Could probably be tidied up.

---
+++ AdventOfCode/Day2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode
+{
+    public class Day2
+    {
+        public static int calculateCheksum(String input)
+        {
+            int runningTotal = 0;
+            string[] listOfLines = input.Split('\n');
+            foreach (string line in listOfLines)
+            {
+                runningTotal += differenceBetweenLargestAndSmallest(line);
+            }
+            return runningTotal;
+        }
+
+        public static int differenceBetweenLargestAndSmallest(String oneRowInput)
+        {
+            char[] v = new[] {'\t', ' '};
+            string[] strings = oneRowInput.Split(v);
+            int currentHighest = 0;
+            int currentLowest = int.MaxValue;
+
+            for (var i = 0; i < strings.Length; i++)
+            {
+                string numberString = strings[i];
+                int currentValue = int.Parse(numberString);
+
+                if (currentValue < currentLowest)
+                {
+                    currentLowest = currentValue;
+                }
+
+                if (currentValue > currentHighest)
+                {
+                    currentHighest = currentValue;
+                }
+            }
+            return currentHighest - currentLowest;
+        }
+    }
+}
