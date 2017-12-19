681a66f29917392ac5dd0c2af2c4ae0a3554cbd3
andrew-m <andrew-m@users.noreply.github.com>
2017-12-19 23:18:57 +0000

    Day 3 slow progress. Useful spreadsheet though.

---
+++ AdventOfCode/Day3Calculators.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day3Calculators
+    {
+        
+        public static int GetLayer(int inputNumber)
+        {
+            if (inputNumber <= 1)
+            {
+                return 0;
+            }
+
+            int currentLayer = 1;
+            
+            while (true)
+            {
+                if (MaxValueInLayer(currentLayer) >= inputNumber)
+                {
+                    return currentLayer;
+                }
+                currentLayer++;
+            }
+        }
+
+        public static int MaxValueInLayer(int layerIndex)
+        {
+            if (layerIndex == 0)
+            {
+                return 1;
+            }
+
+            return (((int)(Math.Pow(layerIndex, 2)) + layerIndex) / 2 * 8) + 1;
+        }
+
+        public static int SizeOfLayer(int LayerIndex)
+        {
+            if (LayerIndex == 0)
+            {
+                return 1;
+            }
+            
+            return LayerIndex * 8;
+        }
+
+        public static IEnumerable<int> Range(int start, int endInclusive)
+        {
+            return Enumerable.Range(start, endInclusive - (start -1));
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day3CalcullatorsTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using AdventOfCode;
+using Xunit;
+
+namespace AdventOfCodeTests
+{
+    public class Day3CalcullatorsTests
+    {
+        [Fact]
+        public void ShouldCalculateLayerFor1()
+        {
+            int result = Day3Calculators.GetLayer(1);
+            
+            Assert.Equal(0, result);
+        }
+        
+        [Fact]
+        public void ShouldCalculateLayerFor2To9()
+        {
+            var range = Day3Calculators.Range(2, 9);
+            var expectedLayer = 1;
+
+            assertValuesInRangeAreInLayer(range, expectedLayer);
+        }
+
+        [Fact]
+        public void ShouldCalculateLayerFor10To25()
+        {
+            var range = Day3Calculators.Range(10, 25);
+            var expectedLayer = 2;
+
+            assertValuesInRangeAreInLayer(range, expectedLayer);
+        }
+        
+        private static void assertValuesInRangeAreInLayer(IEnumerable<int> range, int expectedLayer)
+        {
+            foreach (int n in range)
+            {
+                int result = Day3Calculators.GetLayer(n);
+                Assert.True(expectedLayer == result, $"For input:{n} Expected layer:{expectedLayer} but was layer:{result}");
+            }
+        }
+        
+        [Fact]
+        public void ShouldReturnZerothLayerSizeAs1()
+        {
+            Assert.Equal(1, Day3Calculators.SizeOfLayer(0));
+        }
+        
+        [Fact]
+        public void ShouldReturnFirstLayerSizeAs8()
+        {
+            Assert.Equal(8, Day3Calculators.SizeOfLayer(1));
+        }
+        
+        [Fact]
+        public void ShouldReturnSecondLayerSizeAs16()
+        {
+            Assert.Equal(16, Day3Calculators.SizeOfLayer(2));
+        }
+        
+        [Fact]
+        public void ShouldDoRangeFromXtoY()
+        {
+            int minSeen = 30;
+            int maxSeen = 30;
+            
+            IEnumerable<int> enumerable = Day3Calculators.Range(23, 37);
+            foreach (var i in enumerable)
+            {
+                if (i < minSeen)
+                {
+                    minSeen = i;
+                } else if (i > maxSeen)
+                {
+                    maxSeen = i;
+                }
+            }
+            Assert.Equal(23, minSeen);
+            Assert.Equal(37, maxSeen);
+        }
+        
+        [Fact]
+        public void MaxValueInLayer0_ShouldBe1()
+        {
+            Assert.Equal(1, Day3Calculators.MaxValueInLayer(0));
+        }
+        
+        [Fact]
+        public void MaxValueInLayer1_ShouldBe9()
+        {
+            Assert.Equal(9, Day3Calculators.MaxValueInLayer(1));
+        }
+    }
+}
